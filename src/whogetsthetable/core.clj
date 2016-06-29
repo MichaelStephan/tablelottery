84434657951e10fb71481852cfa5827887445163
Michael Stephan <michael.stephan@sap.com>
2016-06-29 08:54:08 +0200

    initil upload

---
+++ src/whogetsthetable/core.clj
@@ -0,0 +1,19 @@
+(ns whogetsthetable.core
+  (:gen-class))
+
+;|--------------|
+;     T4 -  T1  |
+;     T5 T3 T2  |
+;|              |
+;|--------------|
+
+(def tables #{:t1-at-window :t2-at-window :t3-in-the-middle :t4-in-the-front :t5-in-the-front})
+(def locals #{:HB :IG :AT :RH})
+(def external #{:MS :GH})
+
+(defn -main []
+  (let [locals-cnt (count locals)
+        tables-for-locals (take locals-cnt tables)]
+        (map vector
+         (shuffle locals)
+         (shuffle tables-for-locals))))
---
+++ test/whogetsthetable/core_test.clj
@@ -0,0 +1,7 @@
+(ns whogetsthetable.core-test
+  (:require [clojure.test :refer :all]
+            [whogetsthetable.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
