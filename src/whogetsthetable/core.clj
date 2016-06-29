6a79f72d41912a6cb7e4ef4bcc0ce1cd03842ef0
Michael Stephan <michael.stephan@sap.com>
2016-06-29 13:09:13 +0200

    fixed an issue, sets are not ordered

--- src/whogetsthetable/core.clj
+++ src/whogetsthetable/core.clj
@@ -8,13 +8,15 @@
 ;|              |
 ;|--------------|
 
-(def tables #{:t1-at-window :t2-at-window :t3-in-the-middle :t4-in-the-front :t5-in-the-front})
-(def locals #{:HB :IG :AT :RH})
-(def externals #{:MS :GH})
+(def tables [:t1-at-window :t2-at-window :t3-in-the-middle :t4-in-the-front :t5-in-the-front])
+(def locals [:HB :IG :AT :RH])
+(def externals [:MS :GH])
 
 (defn -main []
   (let [locals-cnt (count locals)
         tables-for-locals (take locals-cnt tables)]
-        (map vector
-         (shuffle locals)
-         (shuffle tables-for-locals))))
+        (->
+          (map vector
+           (shuffle locals)
+           (shuffle tables-for-locals))
+          (println))))
